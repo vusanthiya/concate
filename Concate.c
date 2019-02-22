b40d99d1ec1b795ce44aa36967f00d867b13cfc1
vusanthiya <vusanthiya@gmail.com>
2019-02-22 15:45:56 +0530

    Update Concate.c

--- Concate.c
+++ Concate.c
@@ -2,10 +2,16 @@
 void main()
 {
 clrscr();
-string a="hello";
-string b="world";
-string c=a+b;
-print(c);
+string str1="hello";
+string str2="world";
+  for(int i=0;str1[i]!='\0';++i)
+  {
+    for(int i=0;str2[i]!='\0';++j;++i)
+    {
+      str1[i]=str2[i];
+    }
+  }
+print("%s",str1);
 getch();
 }
 
